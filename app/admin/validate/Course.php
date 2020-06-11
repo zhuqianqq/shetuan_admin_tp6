990a9f9ebd05ddeba8db5c2fca22bf75fe250f24
ouyang <xibiao_ouyang@163.com>
2020-06-11 15:37:35 +0800

    添加课程bug

--- app/admin/validate/Course.php
+++ app/admin/validate/Course.php
@@ -15,30 +15,30 @@ class Course extends Validate
 {
     //验证规则
     protected $rule = [
-        'course_name' => ['require', 'max' => '24'],
+        'courseName' => ['require', 'max' => '24'],
         'status' => ['require'],
         'weeks' => ['require'],
-        'start_time'    => ['require'],
-        'end_time'    => ['require'],
-        'class_place'   => ['require'],
+        'startTime'    => ['require'],
+        'endTime'    => ['require'],
+        'classPlace'   => ['require'],
         'grade'   => ['require']
     ];
 
     //提示信息
     protected $message = [
-        'course_name.require'   => '课程名称必须',
-        'course_name.max'       => '课程名称最多不能超过24个字符',
+        'courseName.require'   => '课程名称必须',
+        'courseName.max'       => '课程名称最多不能超过24个字符',
         'status' => '课程状态必须',
         'weeks'    => '课程安排必须选择',
-        'start_time'           => '请选择课程开始时间',
-        'end_time'           => '请选择课程结束时间',
-        'class_place'        => '请选择课程地点',
+        'startTime'           => '请选择课程开始时间',
+        'endTime'           => '请选择课程结束时间',
+        'classPlace'        => '请选择课程地点',
         'grade'              => '请选择年级',
     ];
 
     //验证场景
     protected $scene = [
-        'save' => ['course_name', 'status', 'weeks', 'start_time', 'end_time', 'class_place', 'grade'],
+        'save' => ['courseName', 'status', 'weeks', 'startTime', 'endTime', 'classPlace', 'grade'],
     ];
 
 
